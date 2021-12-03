a61e1e0d398f612facf7e35db2fdd186a92e01d9
Ulfurerulfur <utbodvarsson@gmail.com>
2021-12-03 11:01:50 +0000

    started styling the login/signup

--- App.js
+++ App.js
@@ -9,20 +9,25 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/LoginScreen";
+import SignupScreen from "./screens/SignupScreen";
+import StartScreen from "./screens/StartScreen"
 import NavBar from "./components/NavBar";
 
 const Stack = createNativeStackNavigator();
 
+
 export default App = () => {
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          options={{ headerShown: false }}
-          name="Login"
-          component={LoginScreen}
+          name="StartScreen"
+          component={StartScreen}
         />
         <Stack.Screen name="App" component={NavBar} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignupScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screens/StartScreen.js
@@ -0,0 +1,153 @@
+
+// ---------- THIS IS THE START SCREEN ---------- \\
+import { useNavigation } from "@react-navigation/core";
+import React, { useEffect, useState } from "react";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Image,
+  Button,
+
+} from "react-native";
+import { auth } from "../config/firebase";
+// Fonts
+import {useFonts} from "expo-font"
+import {AppLoading} from "expo-app-loading"
+// Icons
+import { Icon } from 'react-native-elements'
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigation = useNavigation();
+
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigation.replace("App");
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const handleSignUp = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log("Registered with:", user.email);
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log("Logged in with:", user.email);
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  
+  // For fonts
+  let [fontsLoaded] = useFonts({
+    "bungee": require("./../assets/fonts/Bungee.ttf"),
+    "bungeeHairline": require("./../assets/fonts/BungeeHairline.ttf"),
+    "degularDisplay": require("./../assets/fonts/DegularDisplay-Light.otf"),
+  })
+  
+  return (
+    <View style={styles.body}>
+      <View style={styles.logo}>
+        <Image
+            source={require("../assets/picref/startLogo.png")}
+          />
+      </View>
+
+        <View style={styles.buttonContainer}>
+        <TouchableOpacity 
+          onPress= {() => {
+            navigation.navigate("Login")
+          }}
+
+          style={styles.SignInButton}>
+              <Text 
+                style={styles.SignInButtonText}> 
+              Log in
+              </Text>
+          </TouchableOpacity>
+                  <TouchableOpacity 
+                    onPress={() => {
+                      navigation.navigate("SignUp")
+                    }}
+                    style={styles.SignUpButton}>
+              <Text 
+                style={styles.SignUpButtonText}> 
+              Sign up
+              </Text>
+          </TouchableOpacity>
+        </View>
+    </View>
+
+
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+
+  body: {
+    backgroundColor: "#30361E",
+    flex: 1,
+  },
+  logo: {
+    marginTop: 250,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    marginTop: 50,
+    alignItems: "center",
+  },
+  SignInButton: {
+    backgroundColor: "#FFFDF6",
+    borderColor: "#30361E",
+    borderWidth: 2.3,
+    height: 60,
+    width: 190,
+    marginBottom: 10,
+    borderRadius: 99,
+    alignItems: "center",
+    justifyContent: "center",
+  },   SignInButtonText: {
+        fontFamily: "degularDisplay",
+        color: "#30361E",
+        fontSize: 26,
+        marginLeft: 10,
+  },
+  SignUpButton: {
+    backgroundColor: "#30361E",
+    borderColor: "#FFFDF6",
+    borderWidth: 2.3,
+    height: 60,
+    width: 190,
+    borderRadius: 99,
+    alignItems: "center",
+    justifyContent: "center",
+  },   SignUpButtonText: {
+        fontFamily: "degularDisplay",
+        color: "#FFFDF6",
+        fontSize: 26,
+        marginLeft: 10,
+  },
+
+});
