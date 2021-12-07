8fecd444fd36782d47c9b741354494761efa42c8
Attila <71675386+Attila-101@users.noreply.github.com>
2021-12-07 22:14:03 +0000

    added top tab navigation to community screen

--- screens/CommunityScreen.js
+++ screens/CommunityScreen.js
@@ -1,13 +1,55 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
+import * as React from 'react';
+import { SafeAreaView, Text, View, Button, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import BackButton from "../components/BackButton";
 
-const CommunityScreen = () => {
+function Neighbours({ navigation }) {
   return (
-    <SafeAreaView>
-      <BackButton />
-    </SafeAreaView>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',}}>
+      <Text>Jakobína Jakobsdóttir</Text>
+      <Text>520 Stig</Text>
+      <Button
+        title="Sjá nánar"
+        /*onPress={() => navigation.navigate('#')}*/
+      />
+    </View>
   );
-};
+}
 
-export default CommunityScreen;
+function Country() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hrafnhildur Björnsdóttir</Text>
+      <Text>1070 Stig</Text>
+      <Button
+        title="Sjá nánar"
+        /*onPress={() => navigation.navigate('BARRTRÉ')}*/
+      />
+    </View>
+  );
+}
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function CommunityScreen() {
+  return (
+    <>
+    <SafeAreaView>
+    <BackButton backCircle={true} />
+  </SafeAreaView>
+    <NavigationContainer independent={true}>
+      <Tab.Navigator style={styles.tabBar}>
+        <Tab.Screen name="NÁGRENNI" component={Neighbours} />
+        <Tab.Screen name="LANDIÐ" component={Country} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  tabs: {
+    justifyContent: "center",
+  },
+});
